e3a70531eb1f3a1456df8597932f94783a33ab93
Anirudh Nukala <anirudh.nukala@gmail.com>
2021-07-26 18:48:09 -0500

    updated settings

--- nbastats/settings.py
+++ nbastats/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gs82+9%y!aq5n_l%gia$o@zks9sz2p64%d5ef+&t(tiske72qc'
+#SECRET_KEY = 'django-insecure-gs82+9%y!aq5n_l%gia$o@zks9sz2p64%d5ef+&t(tiske72qc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['collegechance.herokuapp.com']
 
 
 # Application definition
